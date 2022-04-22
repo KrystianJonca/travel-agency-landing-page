5af5d76a7165938417aed25317d23c4fd5693c4f
Wionek <krystianjonca43@gmail.com>
2022-04-22 22:04:32 +0200

    Added offers

--- src/containers/offer/Offer.jsx
+++ src/containers/offer/Offer.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import offersData from './data';
+import { Service } from '../../components/';
 import './Offer.scss';
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <>
+      <section className="tld__offer" id="offer">
+        <div className="tld__offer-container">
+          <p className="tld__offer-container_toptext">Category</p>
+          <h1>We Offer Best Services </h1>
+          <div className="tld__offer-container_offers">
+            <div>
+              {offersData.slice(0, 2).map((offer) => (
+                <Service {...offer} />
+              ))}
+            </div>
+            <div>
+              {offersData.slice(2, 4).map((offer) => (
+                <Service {...offer} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Offer;

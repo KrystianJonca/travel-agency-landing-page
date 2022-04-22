ec727fe9372909ca0c6c50775da94358d04f75b6
Wionek <krystianjonca43@gmail.com>
2022-04-22 22:03:56 +0200

    Added service card

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import decor from '../../assets/icons/Decor.png';
 import './Service.scss';
 
-const Service = () => {
-  return <nav>Service</nav>;
+const Service = ({ icon, iconAlt, title, text, addDecor }) => {
+  return (
+    <>
+      <div className="tld__service">
+        <div className="tld__service-wrapper">
+          <div className="tld__service-content">
+            <div className="tld__service-content_imgwrapper">
+              <img src={icon} alt={iconAlt} />
+            </div>
+            <h3>{title}</h3>
+            <p>{text}</p>
+          </div>
+        </div>
+        {addDecor && (
+          <img src={decor} alt="Red Box" className="tld__service-decor" />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Service;

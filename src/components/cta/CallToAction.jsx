731759a44cddb717990868cf36736b994ae690b4
Wionek <krystianjonca43@gmail.com>
2022-04-22 20:35:46 +0200

    Added header and cta

--- src/components/cta/CallToAction.jsx
+++ src/components/cta/CallToAction.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
 import './CallToAction.scss';
 
 const CallToAction = () => {
-  return <div>CallToAction</div>;
+  return (
+    <div className="tld__cta">
+      <button>Find out more</button>
+      <div className="tld__cta-video">
+        <AiFillPlayCircle size={50} color="#df6951" />
+        <p>Play demo</p>
+      </div>
+    </div>
+  );
 };
 
 export default CallToAction;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { CallToAction } from '../../components';
+import Image from '../../assets/HeaderImage.png';
 import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="tld__header section_padding" id="home">
+      <div className="tld__header-content">
+        <p className="tld__header-content_toptext">
+          Best Destinations around the world
+        </p>
+        <h1>
+          Travel, <span className="tld__header-content_decore">enjoy</span> and
+          live a new and full life
+        </h1>
+        <p className="tld__header-content_text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
+          at quia? Eius voluptatem eligendi veritatis odio est molestiae vitae
+          nostrum libero perferendis.
+        </p>
+        <CallToAction />
+      </div>
+      <div className="tld__header-image">
+        <img src={Image} alt="Girl sitting on a travel suitcase" />
+      </div>
+    </section>
+  );
 };
 
 export default Header;

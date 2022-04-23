94cf90f2042e9539f4fdb416b8cdde2c6fa810cc
Wionek <krystianjonca43@gmail.com>
2022-04-23 13:27:38 +0200

    Added align left prop

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Heading.scss';
 
-const Heading = ({ heading, toptext }) => {
+const Heading = ({ heading, toptext, alignLeft }) => {
   return (
-    <div className="tld__heading">
+    <div className={`tld__heading ${alignLeft && 'alignleft'}`}>
       <p className="tld__heading-toptext">{toptext}</p>
       <h1>{heading}</h1>
     </div>

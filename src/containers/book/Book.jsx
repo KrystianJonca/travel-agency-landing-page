ed42371d018a57afc698146d59a6759d15427c81
Wionek <krystianjonca43@gmail.com>
2022-04-23 13:47:19 +0200

    Added book section

--- src/containers/book/Book.jsx
+++ src/containers/book/Book.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import BookImage from '../../assets/BookImage.png';
+import { Heading, Step } from '../../components';
+import stepsData from './data';
 import './Book.scss';
 
 const Book = () => {
-  return <div>Book</div>;
+  return (
+    <section className="tld__book section_padding" id="book`  ">
+      <div className="tld__book-content">
+        <Heading
+          toptext="Easy and Fast"
+          heading="Book Your Next Trip In 3 Easy Steps"
+          alignLeft={true}
+        />
+        <div className="tld__book-content_steps">
+          {stepsData.map((step) => (
+            <Step icon={step.icon} title={step.title} text={step.text} />
+          ))}
+        </div>
+      </div>
+      <div className="tld__book-image">
+        <img src={BookImage} alt="Book Infographic" />
+      </div>
+    </section>
+  );
 };
 
 export default Book;
---
+++ src/containers/book/data.js
@@ -0,0 +1,23 @@
+import Info1 from '../../assets/icons/info1.png';
+import Info2 from '../../assets/icons/info2.png';
+import Info3 from '../../assets/icons/info3.png';
+
+const data = [
+  {
+    icon: Info1,
+    title: 'Choose Destination',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus.',
+  },
+  {
+    icon: Info2,
+    title: 'Make Payment',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus.',
+  },
+  {
+    icon: Info3,
+    title: 'Reach Airport on Slected Date',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus.',
+  },
+];
+
+export default data;

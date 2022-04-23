2fbeddd0eae05e43d4577edc6d24a4aaf873c41b
Wionek <krystianjonca43@gmail.com>
2022-04-23 19:35:49 +0200

    Added testimonial data

---
+++ src/containers/testimonials/data.js
@@ -0,0 +1,58 @@
+import alitalia from '../../assets/brands/logo1.png';
+import axon from '../../assets/brands/logo2.png';
+import jetstar from '../../assets/brands/logo3.png';
+import expedia from '../../assets/brands/logo4.png';
+import qantas from '../../assets/brands/logo5.png';
+import person from '../../assets/TestimonialImage.png';
+
+export const testimonials = [
+  {
+    image: person,
+    quote:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus."',
+    name: 'Mike Taylor',
+    location: 'Lahore, Pakistan',
+  },
+  {
+    image: person,
+    quote:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus."',
+    name: 'Chris Jones',
+    location: 'Warsaw, Poland',
+  },
+  {
+    image: person,
+    quote:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus."',
+    name: 'Joe Mama',
+    location: 'New York, USA',
+  },
+];
+
+export const brands = [
+  {
+    logo: alitalia,
+    name: 'Alitalia',
+    link: '#',
+  },
+  {
+    logo: axon,
+    name: 'Axon',
+    link: '#',
+  },
+  {
+    logo: jetstar,
+    name: 'Jetstar',
+    link: '#',
+  },
+  {
+    logo: expedia,
+    name: 'Expedia',
+    link: '#',
+  },
+  {
+    logo: qantas,
+    name: 'Qantas',
+    link: '#',
+  },
+];

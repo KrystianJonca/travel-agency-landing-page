b66e988513d7125d040ac46023c86dfbe85102bb
Wionek <krystianjonca43@gmail.com>
2022-04-23 15:45:53 +0200

    Added newsletter section

--- src/containers/newsletter/Newsletter.jsx
+++ src/containers/newsletter/Newsletter.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import newsletter from '../../assets/icons/newsletter.png';
 import './Newsletter.scss';
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <section className="tld__newsletter section__padding" id="newsletter">
+      <div className="tld__newsletter-wrapper">
+        <div className="tld__newsletter-content">
+          <img src={newsletter} alt="Newsletter icon" />
+          <h3>
+            Subscribe to get information, latest news and other interesting
+            offers about Jadoo
+          </h3>
+          <div className="tld__newsletter-content_input">
+            <input type="email" placeholder="Your email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;

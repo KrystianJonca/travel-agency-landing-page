b5d67525076c408a79ed247c250d69a584a2cf75
Wionek <krystianjonca43@gmail.com>
2022-04-23 22:10:05 +0200

    Added a testimonial component

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Testimonial.scss';
 
-const Testimonial = () => {
-  return <nav>Testimonial</nav>;
+const Testimonial = ({ image, quote, name, location }) => {
+  return (
+    <div className="tld__testimonial">
+      <img src={image} alt="Person" className="tld__testimonial-image" />
+      <div className="tld__testimonial-content">
+        <p className="tld__testimonial-content_quote">{quote}</p>
+        <h4>{name}</h4>
+        <p>{location}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
